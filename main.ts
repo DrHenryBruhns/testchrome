a8fa8a09d27e37db3fbca32f5aa8ebf4a3b46ba1
DrHenryBruhns <71009746+DrHenryBruhns@users.noreply.github.com>
2020-09-09 13:24:33 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
+bluetooth.onBluetoothConnected(function () {
+    connected = true
+    basic.showIcon(IconNames.LeftTriangle)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    connected = false
+    basic.showIcon(IconNames.No)
+})
+let connected = false
 bluetooth.startUartService()
 let counter = 0
 basic.showIcon(IconNames.SmallSquare)
 basic.forever(function () {
-	
+    basic.pause(100)
+    counter += 1
 })
