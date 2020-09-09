9f275820eae39457045ee5246dd09038a8da33e9
DrHenryBruhns <71009746+DrHenryBruhns@users.noreply.github.com>
2020-09-09 14:56:05 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,11 +6,23 @@ bluetooth.onBluetoothDisconnected(function () {
     connected = false
     basic.showIcon(IconNames.No)
 })
+let from_server = ""
 let connected = false
 bluetooth.startUartService()
 let counter = 0
 basic.showIcon(IconNames.SmallSquare)
 basic.forever(function () {
+    from_server = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    if (from_server == "reset") {
+        control.reset()
+    }
+})
+basic.forever(function () {
     basic.pause(100)
     counter += 1
+})
+basic.forever(function () {
+    if (connected == true) {
+        bluetooth.uartWriteString("Test:" + counter)
+    }
 })
